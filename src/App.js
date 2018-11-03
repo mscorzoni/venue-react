60d7736251886289ad15a11ea557cddd8797be7e
Marcos Scorzoni <mlessa@gmail.com>
2018-11-03 11:27:51 +0000

    implement highlight component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
 import VenueNfo from  './components/venueNfo';
+import Hightlights from './components/Highlights';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{height: "1500px", background: 'cornflowerblue'}}>
+      <div className="App" style={{height: "1500px"}}>
         <Header />
         <Featured />
         <VenueNfo />
+        <Hightlights />
       </div>
     );
   }
---
+++ src/components/Highlights/Description.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+  return (
+    <Fade>
+      <div className="center_wrapper">
+        <h2>Highlights</h2>
+        <div className="highlight_description">
+          Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Mé faiz elementum girarzis, nisi eros vermeio. Leite de capivaris, leite de mula manquis sem cabeça.
+        </div>
+      </div>
+    </Fade>
+  );
+};
+
+export default Description;
